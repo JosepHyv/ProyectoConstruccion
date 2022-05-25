ba175cb6578c8b9b432eb14bdfc46fb90ba1e437
Panther <Panther@DESKTOP-G1ONP15>
2022-05-25 15:36:43 -0500

    Cambiar la forma de despiegue de la ventana por la forma acordada

--- ProyectoConstruccion/src/proyectoconstruccion/ProyectoConstruccion.java
+++ ProyectoConstruccion/src/proyectoconstruccion/ProyectoConstruccion.java
@@ -4,9 +4,12 @@
  */
 package proyectoconstruccion;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -19,25 +22,13 @@ import javafx.stage.Stage;
 public class ProyectoConstruccion extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("vistas/FXMLInicioSesion.fxml"));
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Inicio de sesión");
+        stage.show();
     }
 
     /**
