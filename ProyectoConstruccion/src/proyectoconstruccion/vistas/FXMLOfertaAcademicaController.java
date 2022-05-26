9309911c478a0858b1617cac584ec01f4faa7115
Panther <Panther@DESKTOP-G1ONP15>
2022-05-26 08:56:41 -0500

    Generar ventanas del caso de uso Consultar Oferta Academica

--- ProyectoConstruccion/src/proyectoconstruccion/vistas/FXMLOfertaAcademicaController.java
+++ ProyectoConstruccion/src/proyectoconstruccion/vistas/FXMLOfertaAcademicaController.java
@@ -7,7 +7,14 @@ package proyectoconstruccion.vistas;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +23,45 @@ import javafx.fxml.Initializable;
  */
 public class FXMLOfertaAcademicaController implements Initializable {
 
+    @FXML
+    private TableView<?> tbExperiencias;
+    @FXML
+    private TextField tfBuscar;
+    @FXML
+    private TableColumn<?, ?> colNombre;
+    @FXML
+    private TableColumn<?, ?> colNRC;
+    @FXML
+    private TableColumn<?, ?> colProgramaEducativo;
+    @FXML
+    private TableColumn<?, ?> colSemestreRecomendado;
+    @FXML
+    private TableColumn<?, ?> colArea;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        //configurarColumnasTabla();
+        //cargarInformacion();
+    }
+
+    private void configurarColumnasTabla() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void cargarInformacion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
+    private void cerrarVentana(){
+        Stage escenario = (Stage) tfBuscar.getScene().getWindow();
+        escenario.close();
+    }
+    
+    @FXML
+    private void btnSalir(ActionEvent event) {
+        cerrarVentana();
+    }
 }
