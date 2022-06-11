9b308b29d9c393b4cb89996a0054e64aa206dc4e
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-06-10 22:21:09 -0500

    Prototipo de CU03

---
+++ ProyectoConstruccion/src/proyectoconstruccion/vistas/FXMLRegistrarProblemáticaAcadémicaController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package proyectoconstruccion.vistas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author ctr305
+ */
+public class FXMLRegistrarProblemáticaAcadémicaController implements Initializable {
+
+    @FXML
+    private TextField tfNumeroAlumnos;
+    @FXML
+    private TextField tfGravedad;
+    @FXML
+    private TextField tfCategoria;
+    @FXML
+    private TextArea taDescripcion;
+    @FXML
+    private TextArea taSolucion;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //TODO
+    }    
+    
+}
