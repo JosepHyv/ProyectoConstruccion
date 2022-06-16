bd649ef25e5a7ea753f0b5fa015e28b13a06ec25
JosepHyv <josephynimoto@gmail.com>
2022-06-16 10:14:56 -0500

    arreglos en las DAO para recuperar estudiantes

---
+++ ProyectoConstruccion/src/proyectoconstruccion/modelo/DAO/EstudiantesDAO.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoconstruccion.modelo.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javafx.scene.control.Alert;
+import proyectoconstruccion.modelo.ConexionBD;
+import proyectoconstruccion.modelo.pojo.Estudiante;
+import proyectoconstruccion.util.Utilidades;
+import proyectoconstruccion.modelo.pojo.InformacionSesion;
+
+/**
+ *
+ * @author JosepHy
+ */
+public class EstudiantesDAO {
+    public static ArrayList<Estudiante> obtenerEstudiantesPorTutor(){
+        ArrayList<Estudiante> estudiantes = new ArrayList<>();
+        Connection conexionBD = ConexionBD.abrirConexionBD();
+        if(conexionBD != null){
+            String query = "SELECT * FROM estudiante WHERE idTutorAcademico = ?;";
+            try{
+                PreparedStatement configurarConsulta = conexionBD.prepareStatement(query);
+                configurarConsulta.setInt(1,InformacionSesion.getInformacionSesion().getIdUsuario());
+                ResultSet resultadoConsulta = configurarConsulta.executeQuery();
+                while(resultadoConsulta.next()){
+                    Estudiante estudianteTemporal = new Estudiante();
+                    estudianteTemporal.setNombres(resultadoConsulta.getString("nombres"));
+                    estudianteTemporal.setApellidoPaterno(resultadoConsulta.getString("apellidoPaterno"));
+                    estudianteTemporal.setApellidoMaterno(resultadoConsulta.getString("apellidoMaterno"));
+                    estudianteTemporal.setCorreo(resultadoConsulta.getString("correo"));
+                    estudianteTemporal.setSemestre(resultadoConsulta.getString("semestre"));
+                    estudiantes.add(estudianteTemporal);
+                }
+                conexionBD.close();
+            }catch(SQLException ex){
+                Utilidades.mostrarAlerta("Advertencia", "No se puede acceder a la base de datos", Alert.AlertType.ERROR);
+            }
+        }else{
+            estudiantes = null;
+        }
+        return estudiantes;
+    }
+    
+}
--- ProyectoConstruccion/src/proyectoconstruccion/vistas/FXMLInicioDeSesionController.java
+++ ProyectoConstruccion/src/proyectoconstruccion/vistas/FXMLInicioDeSesionController.java
@@ -79,6 +79,7 @@ public class FXMLInicioDeSesionController implements Initializable {
         if(usuarioLogin.getCodigoRespuesta() == Constantes.CODIGO_OPERACION_CORRECTA){
             Utilidades.mostrarAlerta("Usuario Verificado","Bienvenido al sistema.",Alert.AlertType.INFORMATION);
             InformacionSesion.getInformacionSesion().setRol(usuarioLogin.getRol());
+            InformacionSesion.getInformacionSesion().setIdUsuario(usuarioLogin.getIdUsuario());
             irPantallaPrincipal();
         }else if(usuarioLogin.getCodigoRespuesta() == Constantes.CODIGO_CREDENCIALES_INCORRECTAS){
             Utilidades.mostrarAlerta("Credenciales incorrectas","Usuario o contraseña incorrectas.",Alert.AlertType.WARNING);
