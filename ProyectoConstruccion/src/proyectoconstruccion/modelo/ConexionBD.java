3a6a3ea94b107583e09c37c3d1c82d5605859f3a
JosepHyv <josephynimoto@gmail.com>
2022-06-16 01:33:09 -0500

    Inicio Sesion y Conexion Listos

--- ProyectoConstruccion/src/proyectoconstruccion/modelo/ConexionBD.java
+++ ProyectoConstruccion/src/proyectoconstruccion/modelo/ConexionBD.java
@@ -4,33 +4,68 @@
  */
 package proyectoconstruccion.modelo;
 
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Path; 
+import java.io.IOException;
 import java.sql.Connection;
+import java.nio.file.Paths; 
+import java.util.Properties;
+import java.sql.SQLException;
 import java.sql.DriverManager;
+import java.io.FileInputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.FileNotFoundException;
 
 /**
  *
  * @author ctr305
+ * @author JosepHy
  */
 public class ConexionBD {
 
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    /*private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DATABASE = "sistematutorias";
     private static final String HOSTNAME = "localhost";
     private static final String PORT = "3306";
     private static final String URL_CONEXION = "jdbc:mysql://"+HOSTNAME+":"+PORT+"/"+DATABASE+"?serverTimezone=UTC";
 
     private static final String USERNAME = "administrador";
-    private static final String PASSWORD = "";
-
-    public static Connection abrirConexionBD(){
-        Connection conexionBD = null;
-        try{ 
-            Class.forName(DRIVER);
-            conexionBD = DriverManager.getConnection(URL_CONEXION, USERNAME, PASSWORD);
-        }catch(Exception e){
-            e.printStackTrace();
+    private static final String PASSWORD = "";*/
+    
+    //private Connection conexion;
+    
+    
+    public static Connection abrirConexionBD() {
+        Connection conexion = null;
+        try{
+            Path CURRENT_FILE = Paths.get("");
+            String directory = CURRENT_FILE.toAbsolutePath().toString();
+            directory = Paths.get(directory, "src", "proyectoconstruccion", "modelo", "dbconfig.txt").toString();
+            System.err.println("File directory is " + directory); // err for debbug
+            URL url = new File(directory).toURI().toURL();
+            FileInputStream archivoConfiguracion = new FileInputStream(new File(url.getPath()));
+            Properties atributos = new Properties();
+            atributos.load(archivoConfiguracion);
+            archivoConfiguracion.close();
+            Class.forName("java.sql.Driver");
+            String direccionBD = atributos.getProperty("DireccionBD");
+            String usuario = atributos.getProperty("Usuario");
+            String contrasenia = atributos.getProperty("Contrasenia");
+            conexion = DriverManager.getConnection(direccionBD, usuario, contrasenia);
+        }catch (FileNotFoundException fnfException) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, fnfException);
+        } catch (IOException ioException){
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ioException);
+        } catch (ClassNotFoundException cnfException) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, cnfException);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return conexionBD;
+        return conexion;
     }
+    
+    
     
 }
--- ProyectoConstruccion/src/proyectoconstruccion/vistas/FXMLInicioDeSesionController.java
+++ ProyectoConstruccion/src/proyectoconstruccion/vistas/FXMLInicioDeSesionController.java
@@ -4,15 +4,24 @@
  */
 package proyectoconstruccion.vistas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import proyectoconstruccion.util.Utilidades;
+import proyectoconstruccion.util.Constantes;
+import proyectoconstruccion.modelo.DAO.UsuarioDAO;
+import proyectoconstruccion.modelo.pojo.Usuario;
 
 /**
  * FXML Controller class
@@ -42,6 +51,57 @@ public class FXMLInicioDeSesionController implements Initializable {
 
     @FXML
     private void clicIniciarSesion(ActionEvent event) {
+        if(validarCampos())
+        {
+            ValidarUsuarioBD(tfUsuario.getText(), pfPassword.getText());
+        }
     }
     
+    
+    public boolean validarCampos()
+    {
+        boolean validos = true;
+        lbPassword.setText("");
+        lbUsuario.setText("");
+        String MESSAGE = "El campo es obligatorio";
+        
+        if(tfUsuario.getText().isEmpty())
+        {
+            validos = false;
+            lbUsuario.setText(MESSAGE);
+        }
+        
+        if(pfPassword.getText().isEmpty())
+        {
+            validos = false;
+            lbPassword.setText(MESSAGE);
+        }
+        
+        return validos;
+    }
+    
+    private void ValidarUsuarioBD(String username, String password){
+        Usuario usuarioLogin = UsuarioDAO.IniciarSesion(username, password);
+        if(usuarioLogin.getCodigoRespuesta() == Constantes.CODIGO_OPERACION_CORRECTA){
+            Utilidades.mostrarAlerta("Usuario Verificado","Bienvenido al sistema.",Alert.AlertType.INFORMATION);
+            irPantallaPrincipal();
+        }else if(usuarioLogin.getCodigoRespuesta() == Constantes.CODIGO_CREDENCIALES_INCORRECTAS){
+            Utilidades.mostrarAlerta("Credenciales incorrectas","Usuario o contraseña incorrectas.",Alert.AlertType.WARNING);
+        }else if(usuarioLogin.getCodigoRespuesta() == Constantes.CODIGO_ERROR_CONEXIONBD){
+            Utilidades.mostrarAlerta("Error de conexion","No existe conexion con la base de datos.",Alert.AlertType.ERROR);
+        }
+    }
+    
+    private void irPantallaPrincipal(){
+        try{
+        Stage escenarioPrincipal = (Stage) tfUsuario.getScene().getWindow();
+        Scene pantallaAlumnos = new Scene(FXMLLoader.load(getClass().getResource("FXMLMenuCUs.fxml")));
+        escenarioPrincipal.setScene(pantallaAlumnos);
+        escenarioPrincipal.setTitle("Ventana principal");
+        escenarioPrincipal.show();
+        } catch (IOException e) {
+            Utilidades.mostrarAlertaConfirmacion("Error", "No se puede cargar la pantalla principal", Alert.AlertType.ERROR);
+            e.printStackTrace();
+        }
+    }
 }
