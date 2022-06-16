c415828ce40955030ce4388998a8508ec2458423
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-06-16 09:45:47 -0500

    Preparar DAO para paso de IDs.

--- ProyectoConstruccion/src/proyectoconstruccion/modelo/DAO/ProblemáticaAcadémicaDAO.java
+++ ProyectoConstruccion/src/proyectoconstruccion/modelo/DAO/ProblemáticaAcadémicaDAO.java
@@ -23,13 +23,15 @@ public class ProblemáticaAcadémicaDAO {
         if(conexionBD != null){
             try{
                 String sentencia = "INSERT INTO problematicaacademica "
-                        + "(numAlum, descripcion, gravedad, categoria) "
-                        + "VALUES (?, ?, ?, ?)";
+                        + "(numAlum, descripcion, gravedad, categoria, idPeriodo, idReporteTutoria) "
+                        + "VALUES (?, ?, ?, ?, ?, ?)";
                 PreparedStatement configurarConsulta = conexionBD.prepareStatement(sentencia);
                 configurarConsulta.setInt(1, problematicaAcademicaRegistro.getNumAlum());
                 configurarConsulta.setString(2, problematicaAcademicaRegistro.getDescripcion());
                 configurarConsulta.setString(3, problematicaAcademicaRegistro.getGravedad());
                 configurarConsulta.setString(4, problematicaAcademicaRegistro.getCategoria());
+                configurarConsulta.setInt(5, problematicaAcademicaRegistro.getIdPeriodo());
+                configurarConsulta.setInt(6, problematicaAcademicaRegistro.getIdReporteTutoria());
                 int filasAfectadas = configurarConsulta.executeUpdate();
                 respuesta = (filasAfectadas == 1) ? Constantes.CODIGO_OPERACION_CORRECTA : Constantes.CODIGO_OPERACION_DML_FALLIDA;
                 conexionBD.close();
@@ -60,8 +62,8 @@ public class ProblemáticaAcadémicaDAO {
                     problematicaAcademicaTemp.setGravedad(resultadoConsulta.getString("descripcion"));
                     problematicaAcademicaTemp.setCategoria(resultadoConsulta.getString("categoria"));
                     problematicaAcademicaTemp.setSolucion(resultadoConsulta.getString("solucion"));
-                    problematicaAcademicaTemp.setIdPeriodo(null);
-                    problematicaAcademicaTemp.setIdReporteTutoria(null);
+                    problematicaAcademicaTemp.setIdPeriodo(resultadoConsulta.getInt("idPeriodo"));
+                    problematicaAcademicaTemp.setIdReporteTutoria(resultadoConsulta.getInt("idReporteTutoria"));
                     problemáticaAcadémicaDB.add(problematicaAcademicaTemp);
                 }
                 conexionBD.close();
