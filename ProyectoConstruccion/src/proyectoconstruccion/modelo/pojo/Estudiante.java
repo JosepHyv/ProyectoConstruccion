dc8d99bf26547085b8d64fff341bba425ad1dc31
JosepHyv <josephynimoto@gmail.com>
2022-06-16 09:18:46 -0500

    estudiante pojo

---
+++ ProyectoConstruccion/src/proyectoconstruccion/modelo/pojo/Estudiante.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoconstruccion.modelo.pojo;
+
+/**
+ *
+ * @author JosepHy
+ */
+public class Estudiante {
+    private int idEstudiante;
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String correo;
+    private String matricula;
+    private String semestre;
+    private int idTutorAcademico;
+    private int idPeriodo;
+    
+    
+    
+    
+}
