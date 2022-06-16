340a71ee72835fd9f1678ad9e884af3032a0976d
Leonardo Criollo <criollo_banneret@tutanota.com>
2022-06-16 14:13:36 -0500

    Corregir.

--- ProyectoConstruccion/src/proyectoconstruccion/modelo/DAO/ProblemáticaAcadémicaDAO.java
+++ ProyectoConstruccion/src/proyectoconstruccion/modelo/DAO/ProblemáticaAcadémicaDAO.java
@@ -23,15 +23,14 @@ public class ProblemáticaAcadémicaDAO {
         if(conexionBD != null){
             try{
                 String sentencia = "INSERT INTO problematicaacademica "
-                        + "(numAlum, descripcion, gravedad, categoria, idPeriodo, idReporteTutoria) "
-                        + "VALUES (?, ?, ?, ?, ?, ?)";
+                        + "(numAlum, descripcion, gravedad, categoria, idPeriodo) "
+                        + "VALUES (?, ?, ?, ?, ?)";
                 PreparedStatement configurarConsulta = conexionBD.prepareStatement(sentencia);
                 configurarConsulta.setInt(1, problematicaAcademicaRegistro.getNumAlum());
                 configurarConsulta.setString(2, problematicaAcademicaRegistro.getDescripcion());
                 configurarConsulta.setString(3, problematicaAcademicaRegistro.getGravedad());
                 configurarConsulta.setString(4, problematicaAcademicaRegistro.getCategoria());
                 configurarConsulta.setInt(5, problematicaAcademicaRegistro.getIdPeriodo());
-                configurarConsulta.setInt(6, problematicaAcademicaRegistro.getIdReporteTutoria());
                 int filasAfectadas = configurarConsulta.executeUpdate();
                 respuesta = (filasAfectadas == 1) ? Constantes.CODIGO_OPERACION_CORRECTA : Constantes.CODIGO_OPERACION_DML_FALLIDA;
                 conexionBD.close();
